0da8ba410f6369472f89be37936e0b6bd7ee518e
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-12 00:13:25 +0200

    fix the main file for now

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import { assert } from "@std/assert";
 // How to use scanner:
 // https://github.com/tailwindlabs/tailwindcss/blob/fac8cd9deff87b387bd745cec9da48834a28df7f/integrations/oxide/wasm.test.ts
 
-assert(typeof Scanner === "function", "Scanner is a function");
+assert(typeof Scanner === "function", "Scanner is not a function");
 
 const scanner = new Scanner({
   sources: [
@@ -17,6 +17,12 @@ const scanner = new Scanner({
   ],
 });
 
-assert(typeof scanner.scan === "function", "scanner.scan is a function");
+assert(typeof scanner.scan === "function", "scanner.scan is not a function");
+assert(
+  typeof scanner.scanSources === "undefined",
+  "scanner.scanSources is not a function",
+);
 
 console.log(JSON.stringify(scanner.scan()));
+
+// console.log(scanner.scan_content(""));
