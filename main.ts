0cd0677d6ec858af067ebb4e013f988a2fdceefc
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-11 23:58:27 +0200

    try to get the scanner to work - status: no error but empty result array from scanner.scan

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
 import { Scanner } from "./tailwindcss-oxide.wasi-browser-deno.ts";
 
+import { join } from "node:path";
+import process from "node:process";
+
 console.log(Scanner);
+
+// How to use scanner:
+// https://github.com/tailwindlabs/tailwindcss/blob/fac8cd9deff87b387bd745cec9da48834a28df7f/integrations/oxide/wasm.test.ts
+
+const scanner = new Scanner({
+  sources: [
+    {
+      base: join(process.cwd(), "src"),
+      pattern: "**/*",
+      negated: false,
+    },
+  ],
+});
+console.log(JSON.stringify(scanner.scan()));
