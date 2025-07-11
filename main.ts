2dd158b420d99184678b12e077a10f69f3cc2eab
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-12 00:04:54 +0200

    still no output to "scan"

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { Scanner } from "./tailwindcss-oxide.wasi-browser-deno.ts";
-
 import { join } from "node:path";
-
-console.log(Scanner);
+import { assert } from "@std/assert";
 
 // How to use scanner:
 // https://github.com/tailwindlabs/tailwindcss/blob/fac8cd9deff87b387bd745cec9da48834a28df7f/integrations/oxide/wasm.test.ts
+
+assert(typeof Scanner === "function", "Scanner is a function");
 
 const scanner = new Scanner({
   sources: [
@@ -16,4 +16,7 @@ const scanner = new Scanner({
     },
   ],
 });
+
+assert(typeof scanner.scan === "function", "scanner.scan is a function");
+
 console.log(JSON.stringify(scanner.scan()));
